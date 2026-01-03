4b8a65124a1c240a79249b28212564ca7f20c61d
suphanuttangwongcharoen <suphanut.tangwong@gmail.com>
2026-01-03 15:55:36 +0700

    nowiknow

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 # print hello world
-print("Hello World")
-
-# print goodbye
-print("good bye")
-
-# print
-print("I'm learning python")
-
-
-# new function
-def add_two(num):
-    return num + 2
-
-print(add_two(2), add_two(5))
+print("Now I know what is git and github")
